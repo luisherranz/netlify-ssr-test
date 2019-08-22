fb7520cde6a2c5c9f4ffe4817f374338a328a2c7
Luis Herranz <luisherranz@gmail.com>
2019-08-22 11:18:02 +0200

    switch to template literal

--- src/functions/server.js
+++ src/functions/server.js
@@ -3,7 +3,9 @@ exports.handler = (event, context, callback) => {
   setTimeout(() => {
     callback(null, {
       statusCode: 200,
-      body: "dynamic page: " + path + " : " + Math.random(),
+      body: `Dynamic page. Url query path: ${path}. Event path: ${
+        event.path
+      }. Random: ${Math.random()}`,
       headers: {
         "Cache-Control": "public, s-maxage=15, stale-while-revalidate=300"
       }
