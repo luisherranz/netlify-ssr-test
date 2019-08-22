4f093ed4981b8287b19e16aad08ff4d2a6c436db
Luis Herranz <luisherranz@gmail.com>
2019-08-22 11:19:26 +0200

    leave only proper path

--- src/functions/server.js
+++ src/functions/server.js
@@ -1,11 +1,8 @@
 exports.handler = (event, context, callback) => {
-  const path = event.queryStringParameters.url;
   setTimeout(() => {
     callback(null, {
       statusCode: 200,
-      body: `Dynamic page. Url query path: ${path}. Event path: ${
-        event.path
-      }. Random: ${Math.random()}`,
+      body: `Dynamic page. Path: ${event.path}. Random: ${Math.random()}`,
       headers: {
         "Cache-Control": "public, s-maxage=15, stale-while-revalidate=300"
       }
