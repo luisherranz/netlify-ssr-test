4b79b645235f1d58d6c7547379bcd951d9659432
Luis Herranz <luisherranz@gmail.com>
2019-08-22 11:16:38 +0200

    try to pass url using query param

--- src/functions/server.js
+++ src/functions/server.js
@@ -1,8 +1,9 @@
 exports.handler = (event, context, callback) => {
+  const path = event.queryStringParameters.url;
   setTimeout(() => {
     callback(null, {
       statusCode: 200,
-      body: "dynamic page: " + event.path + " : " + Math.random(),
+      body: "dynamic page: " + path + " : " + Math.random(),
       headers: {
         "Cache-Control": "public, s-maxage=15, stale-while-revalidate=300"
       }
