a45b45b5529235619c62847c7c8447f5166fa409
Luis Herranz <luisherranz@gmail.com>
2019-08-22 10:52:41 +0200

    add headers to lambda

--- src/functions/new.js
+++ src/functions/new.js
@@ -2,7 +2,10 @@ exports.handler = (event, context, callback) => {
   setTimeout(() => {
     callback(null, {
       statusCode: 200,
-      body: "hi from functions :wave:"
+      body: "hi from functions :wave:",
+      headers: {
+        "Cache-Control": "public, s-maxage=30"
+      }
     });
   }, 3000);
 };
