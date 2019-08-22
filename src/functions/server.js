8e064d1182dff09c939ff0b7df7a399032535d4c
Luis Herranz <luisherranz@gmail.com>
2019-08-22 11:10:55 +0200

    fix server lambda

--- src/functions/server.js
+++ src/functions/server.js
@@ -2,7 +2,7 @@ exports.handler = (event, context, callback) => {
   setTimeout(() => {
     callback(null, {
       statusCode: 200,
-      body: "dynamic page: " + event.pathParameters + " : " Math.random(),
+      body: "dynamic page: " + event.pathParameters + " : " + Math.random(),
       headers: {
         "Cache-Control": "public, s-maxage=15, stale-while-revalidate=300"
       }
